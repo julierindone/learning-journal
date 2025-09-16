f954a60f01f40f0f9184f1749b7c9866e8c14ec6
Julie Rindone <julie.rindone@gmail.com>
2025-09-16 13:44:56 -0700

    Created data.js file, wrote functions to display newest blog posts, added image placeholders.

---
+++ data.js
@@ -0,0 +1,26 @@
+export const blogPostArray = [
+	{
+		title: `A Cool Title`,
+		date: `September 15, 2025`,
+		image: "./images/normal1.jpg",
+		content: "Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.",
+	},
+	{
+		title: `A Cool Title`,
+		date: `September 14, 2025`,
+		image: "./images/normal2.jpg",
+		content: "Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.",
+	},
+	{
+		title: `A Cool Title`,
+		date: `September 13, 2025`,
+		image: "./images/normal3.jpg",
+		content: "Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.",
+	},
+	{
+		title: `A Cool Title`,
+		date: `September 12, 2025`,
+		image: "./images/normal4.jpg",
+		content: "Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.",
+	},
+]
---
+++ index.js
@@ -0,0 +1,48 @@
+import { blogPostArray } from "./data.js"
+
+const newestPost = document.getElementById('newest-post')
+const mainContent = document.getElementById('main-content')
+
+function getNewestPost(){
+	let newestPostObject = blogPostArray[0]
+
+	let cutContent = newestPostObject.content.slice(0, 150)
+
+	newestPost.innerHTML =
+	`
+	<p class="header-post-date">${newestPostObject.date}</p>
+	<h2 class="header-post-title">${newestPostObject.title}</h2>
+	<p class="header-post-content">${cutContent}...(<a href="placeholder.html">Click to continue</a>)</p>
+	`
+	}
+
+function getMainContent() {
+	let mainContentHtml = ''
+	for(let i=1; i < 4; i++) {
+		let currentPost = blogPostArray[i]
+		let cutContent = currentPost.content.slice(0,130)
+		console.log(currentPost.image)
+		console.log(cutContent);
+		mainContentHtml += `
+			<div class="blog-post" id="next-post-${i}">
+				<img class="post-img" alt="image" src="${currentPost.image}">
+				<p class="post-date">${currentPost.date}</p>
+				<p class="post-title">${currentPost.title}</p>
+				<p class="post-content">${cutContent}...(<a href="placeholder.html">Click to continue</a>)</p>
+			</div>
+		`
+	}
+	let viewMore = `
+			<a class="view-more" href="all-posts.html">View more</a>`
+
+		mainContent.innerHTML = mainContentHtml + viewMore
+}
+
+
+function renderHtml() {
+	getNewestPost()
+	getMainContent()
+}
+
+renderHtml()
+
