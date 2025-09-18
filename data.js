cbcfef5287264853c8ab9527b4e50c166cfff169
Julie Rindone <julie.rindone@gmail.com>
2025-09-18 16:31:56 -0700

    Added about content array to data.js (and changed dummy text to lorem Smithsum)

--- data.js
+++ data.js
@@ -1,26 +1,59 @@
 export const blogPostArray = [
 	{
-		title: `A Cool Title`,
+		title: `A Rush and a Push and the Land is Ours`,
 		date: `September 15, 2025`,
 		image: "./images/normal1.jpg",
-		content: "Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.",
+		content: `They said: "There's too much caffeine, in your bloodstream, and a lack of real spice, in your life." I said: "Leave me alone, because I'm alright, dad, surprised to still be on my own..." Oh, but don't mention love, I'd hate the strain of the pain again, a rush and a push and the land that, we stand on is ours. It has been before, so it shall be again, and people who are uglier than you and I, they take what they need, and just leave.
+
+		Oh, but don't mention love, I'd hate the pain of the strain all over again, a rush and a push and the land that, we stand on is ours, it has been before, so why can't it be now? And people who are weaker than you or I, they take what they want from life. Oh, but don't mention love, no - no, don't mention love! `,
 	},
 	{
-		title: `A Cool Title`,
+		title: `Heaven Knows I'm Miserable Now`,
 		date: `September 14, 2025`,
 		image: "./images/normal2.jpg",
-		content: "Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.",
+		content: `I was happy in the haze of a drunken hour, but heaven knows I'm miserable now, I was looking for a job, and then I found a job, and heaven knows I'm miserable now. In my life, why do I give valuable time, to people who don't care if I live or die.
+
+		Two lovers entwined pass me by, and heaven knows I'm miserable now, I was looking for a job, and then I found a job, and heaven knows I'm miserable now. In my life, why do I give valuable time, to people who don't care if I live or die, what she asked of me at the end of the day. Caligula would have blushed, "You've been the house too long" she said, and I naturally fled.`,
 	},
 	{
-		title: `A Cool Title`,
+		title: `Well, I Wonder`,
 		date: `September 13, 2025`,
 		image: "./images/normal3.jpg",
-		content: "Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.",
+		content: `Well I wonder, do you hear me when you sleep? I hoarsely cry. Well I wonder, do you see me when we pass? I half-die. Please keep me in mind, please keep me in mind.
+
+		Gasping - but somehow still alive, this is the fierce last stand of all I am. Gasping - dying - but somehow still alive, this is the final stand of all I am, please keep me in mind.`,
 	},
 	{
-		title: `A Cool Title`,
+		title: `Is It Really So Strange?`,
 		date: `September 12, 2025`,
 		image: "./images/normal4.jpg",
-		content: "Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.",
+		content: `I left the North, I travelled South, I found a tiny house, and I can't help the way I feel. Oh yes, you can kick me, and you can punch me, and you can break my face, but you won't change the way I feel, 'cause I love you, oh...
+
+		And is it really so strange? Oh, is it really so strange? Oh, is it really so, really so strange? I say NO, you say YES, (and you will change your mind.) I left the South, I travelled North, I got confused - I killed a horse, I can't help the way I feel. Oh yes, you can punch me, and you can butt me, and you can break my spine, but you won't change the way I feel, 'cause I love you, oh...`,
 	},
+]
+
+export const aboutMeArray = [
+	{
+		section: "Tech Journey Summary",
+		title: `A Long Journet Into Tech`,
+		latestUpdate: `09/18/2025`,
+		content: `I know I'm unloveable, you don't have to tell me, I don't have much in my life, but take it - it's yours, I know I'm unloveable, you don't have to tell me, message received, loud and clear, I don't have much in my life, but take it - it's yours, I wear Black on the outside, because Black is how I feel on the inside, I wear Black on the outside, because Black is how I feel on the inside, and if I seem a little strange, well, that's because I am, if I seem a little strange, that's because I am.`,
+	},
+	{
+		section: `Stuff I like?`,
+		title: `Undecided Section Title`,
+		latestUpdate: `09/17/2025`,
+		content: `Punctured bicycle, on a hillside desolate, will nature make a man of me? NO. When in this charming car, this charming man.
+
+		Why pamper life's complexity, when the leather runs smooth, on the passenger's seat? I would go out tonight, but I haven't got a stitch to wear, this man said, "It's gruesome, that someone so handsome should care." Ah, a jumped-up country boy, who never knew his place, he said, "Return the ring," He knows so much about these things, he knows so much about these things.`,
+	},
+	{
+		section: `No Idea`,
+		title: `Really? A third thing?! JFC I'm obsessed with myself.`,
+		latestUpdate: `09/17/2025`,
+		content: `This is the last night of the fair, and the grease in the hair, of a speedway operator, is all a tremulous heart requires, a schoolgirl is denied, she said: "How quickly would I die, if I jumped from the top of the parachutes?" This is the last night of the fair, and the grease in the hair, of a speedway operator, is all a tremulous heart requires, a schoolgirl is denied, she said: "How quickly would I die, oh, if I jumped from the top of the parachutes?"
+
+		Oh, walk home alone, I might walk home alone, but my faith in love is still devout, I might walk home alone, but my faith in love is still devout, I might walk home alone, but my faith in love is still devout.`,
+	}
 ]
