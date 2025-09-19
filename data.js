3c971cafac94b519d40c90473261ed5aa27f4999
Julie Rindone <julie.rindone@gmail.com>
2025-09-18 18:05:40 -0700

    Updated homepage so newest blog post's image automatically displays in header

--- data.js
+++ data.js
@@ -2,7 +2,7 @@ export const blogPostArray = [
 	{
 		title: `A Rush and a Push and the Land is Ours`,
 		date: `September 15, 2025`,
-		image: "./images/normal1.jpg",
+		image: "./images/rainbow_keyboard.jpg",
 		content: `They said: "There's too much caffeine, in your bloodstream, and a lack of real spice, in your life." I said: "Leave me alone, because I'm alright, dad, surprised to still be on my own..." Oh, but don't mention love, I'd hate the strain of the pain again, a rush and a push and the land that, we stand on is ours. It has been before, so it shall be again, and people who are uglier than you and I, they take what they need, and just leave.
 
 		Oh, but don't mention love, I'd hate the pain of the strain all over again, a rush and a push and the land that, we stand on is ours, it has been before, so why can't it be now? And people who are weaker than you or I, they take what they want from life. Oh, but don't mention love, no - no, don't mention love! `,
@@ -10,7 +10,7 @@ export const blogPostArray = [
 	{
 		title: `Heaven Knows I'm Miserable Now`,
 		date: `September 14, 2025`,
-		image: "./images/normal2.jpg",
+		image: "./images/normal1.jpg",
 		content: `I was happy in the haze of a drunken hour, but heaven knows I'm miserable now, I was looking for a job, and then I found a job, and heaven knows I'm miserable now. In my life, why do I give valuable time, to people who don't care if I live or die.
 
 		Two lovers entwined pass me by, and heaven knows I'm miserable now, I was looking for a job, and then I found a job, and heaven knows I'm miserable now. In my life, why do I give valuable time, to people who don't care if I live or die, what she asked of me at the end of the day. Caligula would have blushed, "You've been the house too long" she said, and I naturally fled.`,
@@ -18,7 +18,7 @@ export const blogPostArray = [
 	{
 		title: `Well, I Wonder`,
 		date: `September 13, 2025`,
-		image: "./images/normal3.jpg",
+		image: "./images/normal2.jpg",
 		content: `Well I wonder, do you hear me when you sleep? I hoarsely cry. Well I wonder, do you see me when we pass? I half-die. Please keep me in mind, please keep me in mind.
 
 		Gasping - but somehow still alive, this is the fierce last stand of all I am. Gasping - dying - but somehow still alive, this is the final stand of all I am, please keep me in mind.`,
@@ -26,10 +26,38 @@ export const blogPostArray = [
 	{
 		title: `Is It Really So Strange?`,
 		date: `September 12, 2025`,
-		image: "./images/normal4.jpg",
+		image: "./images/normal3.jpg",
 		content: `I left the North, I travelled South, I found a tiny house, and I can't help the way I feel. Oh yes, you can kick me, and you can punch me, and you can break my face, but you won't change the way I feel, 'cause I love you, oh...
 
 		And is it really so strange? Oh, is it really so strange? Oh, is it really so, really so strange? I say NO, you say YES, (and you will change your mind.) I left the South, I travelled North, I got confused - I killed a horse, I can't help the way I feel. Oh yes, you can punch me, and you can butt me, and you can break my spine, but you won't change the way I feel, 'cause I love you, oh...`,
+	},
+	{
+		title: `What Difference Does it Make?`,
+		date: `September 11, 2025`,
+		image: "./images/normal4.jpg",
+		content: `All men have secrets and here is mine, so let it be known, for we have been through hell and high tide, I think I can rely on you... And yet you start to recoil, heavy words are so lightly thrown, but still I'd leap in front of a flying bullet for you. So, what difference does it make? So, what difference does it make? It makes none, but now you have gone, and you must be looking very old tonight.
+
+		Oh, what difference does it make? Oh, it makes none, but now you have gone, and your prejudice won't keep you warm tonight. Oh, the devil will find work for idle hands to do, I stole, and then I lied, just because you asked me to, but now you know the truth about me, you won't see me anymore, well, I'm still fond of you, oh-ho-oh.`,
+	},
+	{
+		title: `Paint a Vulgar Picture`,
+		date: `September 10, 2025`,
+		image: "./images/normal5.jpg",
+		content: `At the record company meeting, on their hands - a dead star, and oh, the plans they weave, and oh, the sickening greed. At the record company party, on their hands - a dead star, the sycophantic slags all say: "I knew him first, and I knew him well."
+
+		Re-issue! Re-package! Re-package! Re-evaluate the songs, double-pack with a photograph, extra track (and a tacky badge.) A-list, playlist, "Please them, please them!" "Please them!" (sadly, this was your life.) But you could have said no, if you'd wanted to, you could have said no, if you'd wanted to.
+
+		BPI, MTV, BBC, "Please them! Please then!" (sadly this was your life.) But you could have said no, if you'd wanted to, you could have walked away, ... Couldn't you?
+
+		I touched you at the soundcheck, you had no real way of knowing, in my heart I begged "Take me with you... I don't care where you're going..." But to you I was faceless, I was fawning, I was boring, just a child from those ugly new houses, who could never begin to know. Who could never really know, oh...`,
+	},
+	{
+		title: `Unhappy Birthday`,
+		date: `September 19, 2025`,
+		image: "./images/normal6.jpg",
+		content: `I've come to wish you an unhappy birthday, I've come to wish you an unhappy birthday, 'cause you're evil, and you lie, and if you should die, I may feel slightly sad, (but I won't cry.) Loved and lost, and some may say, "When usually it's nothing, surely you're happy, it should be this way!" I say: "No, I'm gonna kill my dog," And: "May the lines sag, may the lines sag, heavy and deep tonight."
+
+		I've come to wish you an unhappy birthday, I've come to wish you an unhappy birthday, 'cause you're evil, and you lie, and if you should die, I may feel slightly sad, (but I won't cry.) Loved and lost, and some may say: "When usually it's nothing, surely you're happy, it should be this way!" I said "No," And then I shot myself, so, drink, drink, drink, and be ill tonight. From the one you left behind, from the one you left behind, from the one you left behind, from the one you left behind.`,
 	},
 ]
 
@@ -55,5 +83,5 @@ export const aboutMeArray = [
 		content: `This is the last night of the fair, and the grease in the hair, of a speedway operator, is all a tremulous heart requires, a schoolgirl is denied, she said: "How quickly would I die, if I jumped from the top of the parachutes?" This is the last night of the fair, and the grease in the hair, of a speedway operator, is all a tremulous heart requires, a schoolgirl is denied, she said: "How quickly would I die, oh, if I jumped from the top of the parachutes?"
 
 		Oh, walk home alone, I might walk home alone, but my faith in love is still devout, I might walk home alone, but my faith in love is still devout, I might walk home alone, but my faith in love is still devout.`,
-	}
+	},
 ]
