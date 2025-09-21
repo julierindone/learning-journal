9c88e89949df686dec87b1ef7ecf85ac7d5668ec
Julie Rindone <julie.rindone@gmail.com>
2025-09-21 12:20:01 -0700

    Added next 3 blog entries to main section, updated some classes/IDs /css for clarity or reuse

--- blog.js
+++ blog.js
@@ -1,9 +1,9 @@
 import { blogPostArray } from "./data.js"
 
-function getFeaturedPost () {
-	console.log("in getFeaturedPost")
-	let featuredPost = blogPostArray[0]
+const mainBlogContent = document.getElementById('main-blog-content')
 
+function getFeaturedPost() {
+	let featuredPost = blogPostArray[0]
 
 	let featuredBlogImage = document.getElementById('featured-blog-image')
 	featuredBlogImage.src = featuredPost.image
@@ -17,8 +17,31 @@ function getFeaturedPost () {
 
 	let featuredBlogContent = document.getElementById('featured-blog-content')
 	featuredBlogContent.innerText = featuredPost.content
-
 }
 
-getFeaturedPost()
+function getMainBlogContent() {
+	let mainContentHtml = ''
+	for (let i = 1; i < 4; i++) {
+		let currentPost = blogPostArray[i]
+		let cutContent = currentPost.content.slice(0, 200)
 
+		mainContentHtml += `
+			<div class="blog-post" id="next-post-${i}">
+				<img class="post-img" alt="image" src="${currentPost.image}">
+				<p class="post-date">${currentPost.date}</p>
+				<p class="post-title">${currentPost.title}</p>
+				<p class="post-content">${cutContent}...(<a href="placeholder.html">Click to continue</a>)</p>
+			</div>
+		`
+	}
+	let viewMore = `<a class="view-more" href="all-posts.html">View more</a>`
+
+	mainBlogContent.innerHTML = mainContentHtml + viewMore
+}
+
+function renderBlogContent() {
+	getFeaturedPost()
+	getMainBlogContent()
+}
+
+renderBlogContent()
--- index.js
+++ index.js
@@ -1,32 +1,33 @@
 import { blogPostArray } from "./data.js"
 
 const mainHomeContent = document.getElementById('main-home-content')
-const newestPost = document.getElementById('newest-post')
+const featuredPost = document.getElementById('featured-post')
 
-function getNewestPost() {
-	let newestPostObject = blogPostArray[0]
-	let cutContent = newestPostObject.content.slice(0, 150)
+function getFeaturedPost() {
+	let featuredPostObject = blogPostArray[0]
+	let cutContent = featuredPostObject.content.slice(0, 150)
 
-	newestPost.innerHTML = `
-	<p class="header-post-date">${newestPostObject.date}</p>
-	<h1 class="header-post-title">${newestPostObject.title}</h1>
+	featuredPost.innerHTML = `
+	<p class="header-post-date">${featuredPostObject.date}</p>
+	<h1 class="header-post-title">${featuredPostObject.title}</h1>
 	<p class="header-post-content">${cutContent}...(<a href="placeholder.html">Click to continue</a>)</p>`
 
-	document.getElementsByClassName('site-header')[0].style.cssText = `
-	background: linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.95)), url("${newestPostObject.image}");
-	background-repeat: no-repeat;
-	background-position: center;
-	background-size: cover;`
+	let backgroundCss = `
+		background: linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0.95)),
+			url("${featuredPostObject.image}");
+		background-repeat: no-repeat;
+		background-position: center;
+		background-size: cover;`
+	document.getElementById('home-header').style.cssText = backgroundCss
 }
 
-function getmainHomeContent() {
-	let mainHomeContentHtml = ''
+function getMainHomeContent() {
+	let mainContentHtml = ''
 	for (let i = 1; i < 4; i++) {
 		let currentPost = blogPostArray[i]
 		let cutContent = currentPost.content.slice(0, 200)
-		console.log(currentPost.image)
-		console.log(cutContent);
-		mainHomeContentHtml += `
+
+		mainContentHtml += `
 			<div class="blog-post" id="next-post-${i}">
 				<img class="post-img" alt="image" src="${currentPost.image}">
 				<p class="post-date">${currentPost.date}</p>
@@ -35,15 +36,14 @@ function getmainHomeContent() {
 			</div>
 		`
 	}
-	let viewMore = `
-			<a class="view-more" href="all-posts.html">View more</a>`
+	let viewMore = `<a class="view-more" href="all-posts.html">View more</a>`
 
-	mainHomeContent.innerHTML = mainHomeContentHtml + viewMore
+	mainHomeContent.innerHTML = mainContentHtml + viewMore
 }
 
 function renderHomeContent() {
-	getNewestPost()
-	getmainHomeContent()
+	getFeaturedPost()
+	getMainHomeContent()
 }
 
 renderHomeContent()
