7c2873c4d701dc8b12956d472bd881ba1ecc3c4a
Julie Rindone <julie.rindone@gmail.com>
2025-10-31 14:21:05 -0700

    Added functions and styling for thumbnail grid

--- blog.js
+++ blog.js
@@ -1,17 +1,12 @@
-import { createPostHtml } from "./sharedFunctions.js"
+import { createPostHtml, createThumbnailCardHtml } from "./sharedFunctions.js"
 import { blogPostArray } from "./data.js"
 
 const blogFeaturedPost = document.getElementById('blog-featured-post')
 const blogFullPostWrapper = document.getElementById('blog-full-posts-wrapper')
 let blogThumbnailCardGrid = document.getElementById('blog-thumbnail-card-grid')
+let displayedPostCount = 0
+let unpostedPostCount = blogPostArray.length
 
-function getScreenWidth() {
-	if (window.innerWidth < 800) {
-		screenWidth = "mobile"
-	} else {
-		screenWidth = "desktop"
-	}
-}
 
 function getFeaturedPost() {
 	let featuredPostObject = blogPostArray[0]
@@ -21,7 +16,10 @@ function getFeaturedPost() {
 			<p class="post-date">${featuredPostObject.date}</p>
 			<p class="post-content">${featuredPostObject.content}</p>`
 
-			blogFeaturedPost.innerHTML = featuredPostHtml
+	displayedPostCount++
+	unpostedPostCount -= 1
+
+	blogFeaturedPost.innerHTML = featuredPostHtml
 }
 
 function getFullBlogPosts() {
@@ -29,17 +27,33 @@ function getFullBlogPosts() {
 	for (let i = 1; i < 4; i++) {
 		let postToBuild = blogPostArray[i]
 		fullPostsHtml += createPostHtml(postToBuild, "full")
+		displayedPostCount++
+		unpostedPostCount -= 1
 	}
+
 	blogFullPostWrapper.innerHTML = fullPostsHtml
 }
 
+function getThumbnailCardGrid() {
+	let thumbnailCardHtml = ''
+	// thumbnailStartingIndex keeps track of the index that should be created
+	let thumbnailStartingIndex = displayedPostCount
+
+	// TODO: Cut off at 4 and then add button to load more
+	for (let i = thumbnailStartingIndex; i < blogPostArray.length; i++) {
+		let postToBuild = blogPostArray[i]
+		thumbnailCardHtml += createThumbnailCardHtml(postToBuild)
+		displayedPostCount++
+		unpostedPostCount -= 1
+	}
+
+	blogThumbnailCardGrid.innerHTML = thumbnailCardHtml
 }
 
 function renderBlogContent() {
 	getFeaturedPost()
 	getFullBlogPosts()
-	getPostCardGrid()
+	getThumbnailCardGrid()
 }
 
-getScreenWidth()
 renderBlogContent()
